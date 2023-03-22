a1646831906fa9c68ee7c0690985d7d08b08c8f5
eprader <56026248+eprader@users.noreply.github.com>
2023-03-22 15:57:11 +0100

    add clangformat and modify .gitignore

--- w02/e02/slow.c
+++ w02/e02/slow.c
@@ -1,16 +1,13 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    const int n = 1000;
+int main() {
+  const int n = 1000;
 
-    int a[n][n], b[n][n], c[n][n];
-    for (size_t j = 0; j < n; ++j)
-    {
-        for (size_t i = 0; i < n; ++i)
-        {
-            c[i][j] = a[i][j] * b[i][j];
-        }
+  int a[n][n], b[n][n], c[n][n];
+  for (size_t j = 0; j < n; ++j) {
+    for (size_t i = 0; i < n; ++i) {
+      c[i][j] = a[i][j] * b[i][j];
     }
+  }
 }
