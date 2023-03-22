0c61888786520e373c890a7fae3b8a1dbe0f4eae
eprader <56026248+eprader@users.noreply.github.com>
2023-03-22 16:00:39 +0100

    added clang-format and formatted

--- w02/e02/slow.c
+++ w02/e02/slow.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-  const int n = 1000;
+    const int n = 1000;
 
-  int a[n][n], b[n][n], c[n][n];
-  for (size_t j = 0; j < n; ++j) {
-    for (size_t i = 0; i < n; ++i) {
-      c[i][j] = a[i][j] * b[i][j];
+    int a[n][n], b[n][n], c[n][n];
+    for(size_t j = 0; j < n; ++j) {
+        for(size_t i = 0; i < n; ++i) {
+            c[i][j] = a[i][j] * b[i][j];
+        }
     }
-  }
 }
