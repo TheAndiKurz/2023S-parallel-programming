94f5ca6ca6a997f0dca57c742ed7260f85e1a773
Andreas Kurz <off.andreaskurz+github@gmail.com>
2023-03-26 14:21:24 +0200

    added the team.txt file and formated everything the same

--- w02/e01/code/mandelbrot_op.c
+++ w02/e01/code/mandelbrot_op.c
@@ -5,50 +5,51 @@
 
 // Include that allows to print result as an image
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h" 
 #include "my_timer.h"
+#include "stb_image_write.h"
 
 // Default size of image
 #define X 1920
 #define Y 1080
-//#define X 1280
-//#define Y 720
+// #define X 1280
+// #define Y 720
 #define MAX_ITER 80
 
 #define CX_SCALE 4.9
 #define CX_OFFS 2.8
-#define CY_SCALE 2.8 
+#define CY_SCALE 2.8
 #define CY_OFFS 1.4
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-  for (int i = 0; i < X; i++){
-    for (int j = 0; j < Y; j++){
-      // code adapted from https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
-      float x = 0, y = 0;
-      float x2 = 0, y2 = 0;
-      float cx = ((i/(float)X)*CX_SCALE)-CX_OFFS;
-      float cy = ((j/(float)Y)*CY_SCALE)-CY_OFFS;
-      int iteration = 0;
-      while (x2 + y2 <= 4 && iteration < MAX_ITER) {
-        y = 2*x*y + cy;
-        x = x2 - y2 + cx;
-        x2 = x*x;
-        y2 = y*y; 
-        iteration++;
-      }
-      uint8_t norm_iteration = (iteration/(float)MAX_ITER)*255;
-      image[j][i] = norm_iteration;
+    for(int i = 0; i < X; i++) {
+        for(int j = 0; j < Y; j++) {
+            // code adapted from
+            // https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
+            float x = 0, y = 0;
+            float x2 = 0, y2 = 0;
+            float cx = ((i / (float)X) * CX_SCALE) - CX_OFFS;
+            float cy = ((j / (float)Y) * CY_SCALE) - CY_OFFS;
+            int iteration = 0;
+            while(x2 + y2 <= 4 && iteration < MAX_ITER) {
+                y = 2 * x * y + cy;
+                x = x2 - y2 + cx;
+                x2 = x * x;
+                y2 = y * y;
+                iteration++;
+            }
+            uint8_t norm_iteration = (iteration / (float)MAX_ITER) * 255;
+            image[j][i] = norm_iteration;
+        }
     }
-  }
 }
 
 int main() {
-	uint8_t image[Y][X];
-  double timer = start_to_record();
-	calc_mandelbrot(image);
-  stop_record(timer);
+    uint8_t image[Y][X];
+    double timer = start_to_record();
+    calc_mandelbrot(image);
+    stop_record(timer);
 
-	const int channel_nr = 1, stride_bytes = 0;
-	stbi_write_png("mandelbrot.png", X, Y, channel_nr, image, stride_bytes);
-	return EXIT_SUCCESS;
+    const int channel_nr = 1, stride_bytes = 0;
+    stbi_write_png("mandelbrot.png", X, Y, channel_nr, image, stride_bytes);
+    return EXIT_SUCCESS;
 }
--- w02/e03/serial_monte_carlo.c
+++ w02/e03/serial_monte_carlo.c
@@ -1,24 +1,20 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <time.h>
 
-double rand_double(unsigned *seed)
-{
+double rand_double(unsigned* seed) {
     return (double)rand_r(seed) / (double)RAND_MAX;
 }
 
-double monte_carlo(size_t n)
-{
+double monte_carlo(size_t n) {
     size_t inside = 0;
     unsigned seed = time(NULL);
 
-    for (size_t i = 0; i < n; i++)
-    {
+    for(size_t i = 0; i < n; i++) {
         double x = rand_double(&seed), y = rand_double(&seed);
 
-        if (x * x + y * y <= 1)
-        {
+        if(x * x + y * y <= 1) {
             inside++;
         }
     }
@@ -26,10 +22,8 @@ double monte_carlo(size_t n)
     return (double)(4 * inside) / (double)n;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
         fprintf(stderr, "usage: %s <number-of-points>\n", argv[0]);
         return 1;
     }
@@ -47,7 +41,7 @@ int main(int argc, char *argv[])
     printf("Monte Carlo Pi: %lf\n", pi_aprx);
     printf("time: %lf\n", exc_time);
 
-    FILE *f = fopen("values.csv", "a+");
+    FILE* f = fopen("values.csv", "a+");
 
     fprintf(f, "Serial, %lf, %lf\n", exc_time, pi_aprx);
 
