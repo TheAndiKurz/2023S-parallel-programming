86a055c8bf8278d8b5bd9a472dd3c30a51fd4560
Andreas Kurz <andikurz48@gmail.com>
2023-04-15 11:56:16 +0200

    changed the libraries a little bit implemented the exc 1 from week 3 changed zip makefile to also include the tools folder

---
+++ w03/e01/job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name test
+# Redirect output stream to this file
+#SBATCH --output=output.log
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=1
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=1
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+
+# for threads in [1, 2, 4, 8] use monte_carlo with 500000000 samples
+for threads in 1 2 4 8; do
+  export OMP_NUM_THREADS=$threads
+  echo "threads: $threads"
+  ./monte_carlo_atomic $threads 500000000
+  ./monte_carlo_critical $threads 500000000
+  ./monte_carlo_reduction $threads 500000000
+done
---
+++ w03/e01/monte_carlo.c
@@ -0,0 +1,67 @@
+#include "../../tools/time/time.h"
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef REDUCTION
+#define VARIANT "Reduction"
+#elif CRITICAL
+#define VARIANT "Critical"
+#else
+#define VARIANT "Atomic"
+#endif
+
+double rand_double(unsigned* seed) {
+    return (double)rand_r(seed) / (double)RAND_MAX;
+}
+
+double monte_carlo(size_t threads, size_t n) {
+    size_t inside = 0;
+    unsigned seed = time(NULL);
+#ifdef REDUCTION
+#pragma omp parallel for num_threads(threads) reduction(+ : inside)
+#else
+#pragma omp parallel for num_threads(threads)
+#endif
+    for(size_t i = 0; i < n; i++) {
+        double x = rand_double(&seed), y = rand_double(&seed);
+
+        if(x * x + y * y <= 1) {
+#ifdef CRITICAL
+#pragma omp critical
+#endif
+#ifdef ATOMIC
+#pragma omp atomic
+#endif
+            inside++;
+        }
+    }
+
+    return (double)(4 * inside) / (double)n;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        fprintf(stderr, "usage: %s <number-of-threads> <number-of-points>\n", argv[0]);
+        return 1;
+    }
+
+    int threads = atoi(argv[1]);
+    int n = atoi(argv[2]);
+
+    double startTime = omp_get_wtime();
+
+    double pi_aprx = monte_carlo(threads, n);
+
+    double endTime = omp_get_wtime();
+
+    double exc_time = endTime - startTime;
+
+    printf("Monte Carlo Pi: %lf\n", pi_aprx);
+    printf("time: %lf\n", exc_time);
+
+    add_time(VARIANT, threads, exc_time);
+
+    return 0;
+}
