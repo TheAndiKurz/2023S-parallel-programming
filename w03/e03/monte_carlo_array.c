0115832936e7b893e488092975832bed2b9a3c22
Finn Achatz <finn.achatz@student.uibk.ac.at>
2023-04-16 22:36:56 +0200

    code should be done

---
+++ w03/e03/monte_carlo_array.c
@@ -0,0 +1,57 @@
+#include "../../tools/time/time.h"
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef PADDING
+#define PADDING 0
+#endif /* !PADDING */
+
+double rand_double(unsigned* seed) {
+    return (double)rand_r(seed) / (double)RAND_MAX;
+}
+
+double monte_carlo(size_t threads, size_t n) {
+    void* sum_array = malloc((sizeof(size_t) + PADDING) * threads;
+    unsigned seed = time(NULL);
+#pragma omp parallel for num_threads(threads)
+    for(size_t i = 0; i < n; i++) {
+        double x = rand_double(&seed), y = rand_double(&seed);
+
+        if(x * x + y * y <= 1) {
+            (*(size_t*)(sum_array + (sizeof(size_t) + PADDING) * omp_get_thread_num()))++;
+        }
+    }
+    size_t sum = 0;
+    for (size_t i = 0; i < threads; i++){
+        sum += *(size_t*)(sum_array + (sizeof(size_t) + PADDING) * i);
+    }
+
+    return (double)(4 * sum) / (double)n;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        fprintf(stderr, "usage: %s <number-of-threads> <number-of-points>\n", argv[0]);
+        return 1;
+    }
+
+    int threads = atoi(argv[1]);
+    int n = atoi(argv[2]);
+
+    double startTime = omp_get_wtime();
+
+    double pi_aprx = monte_carlo(threads, n);
+
+    double endTime = omp_get_wtime();
+
+    double exc_time = endTime - startTime;
+
+    printf("Monte Carlo Pi: %lf\n", pi_aprx);
+    printf("time: %lf\n", exc_time);
+
+    add_time(VARIANT, threads, exc_time);
+
+    return 0;
+}
