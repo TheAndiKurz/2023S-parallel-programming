0587efa2b74ee151f0396c9e857163cff607f136
eprader <56026248+eprader@users.noreply.github.com>
2023-04-18 10:59:17 +0200

    update colors

--- w03/e02/plot.py
+++ w03/e02/plot.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import cmasher as cmr
 
 # Load the data from the CSV file
 df = pd.read_csv('times.csv').sort_values(by=['name'], ascending=True)
@@ -9,8 +11,12 @@ df = pd.read_csv('times.csv').sort_values(by=['name'], ascending=True)
 # Group the data by 'threads' and 'name' columns and calculate the mean and standard deviation
 grouped = df.groupby(['threads', 'name'])['time'].agg(['mean', 'std']).unstack().reset_index()
 
+# use the Safe color palette by Paul Tol
+colors = ['#009E73', '#0072B2', '#D55E00', '#CC79A7', '#F0E442', '#56B4E9', '#bcbcbc']
+cmap = ListedColormap(colors)
+
 # Create a grouped bar chart with error bars showing the standard deviation
-ax = grouped['mean'].plot(kind='bar', yerr=grouped['std'], rot=0, cmap='Set1')
+ax = grouped['mean'].plot(kind='bar', yerr=grouped['std'], rot=0, cmap=cmap)
 
 # Set the x-axis tick labels to the 'threads' column values
 ax.set_xticklabels(grouped['threads'])
