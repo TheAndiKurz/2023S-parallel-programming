a2e38395e56c0a7d48e3be9dfc365fb841f9d67d
eprader <56026248+eprader@users.noreply.github.com>
2023-04-24 15:45:55 +0200

    extend explanation

--- w04/e02/flush.c
+++ w04/e02/flush.c
@@ -2,22 +2,30 @@
 #include <stdio.h>
 
 int main() {
-   int data, flag = 0;
-   #pragma omp parallel num_threads(2)
-   {
-      if (omp_get_thread_num()==0) {
-         /* Write to the data buffer that will be read by thread */
-         data = 42;
-         /* Set flag to release thread 1 */
-         flag = 1;
-      }
-      else if (omp_get_thread_num()==1) {
-         /* Loop until we see the update to the flag */
-         while (flag < 1) {
-         }
-         /* print flag and data */
-         printf("flag=%d data=%d\n", flag, data);
-      }
-   }
-   return 0;
+    int data, flag = 0;
+#pragma omp parallel num_threads(2)
+    {
+        if(omp_get_thread_num() == 0) {
+            /* Write to the data buffer that will be read by thread */
+            data = 42;
+#pragma omp flush(data)
+            /* Set flag to release thread 1 */
+            flag = 1;
+#pragma omp flush(flag)
+            /* #pragma omp flush(flag, data) this would seem to work aswell but the order of these
+               flushes is not guaranteed and therefore there could be a race condition again.
+               a very similar example was shown in the lecture */
+
+        } else if(omp_get_thread_num() == 1) {
+            /* Loop until we see the update to the flag */
+#pragma omp flush(flag)
+            while(flag < 1) {
+#pragma omp flush(flag)
+            }
+            /* print flag and data */
+#pragma omp flush(data)
+            printf("flag=%d data=%d\n", flag, data);
+        }
+    }
+    return 0;
 }
