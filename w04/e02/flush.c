a9d335f42fe220882b737b72050ec3ba0f63c96e
eprader <56026248+eprader@users.noreply.github.com>
2023-04-24 14:59:59 +0200

    update and add team

--- w04/e02/flush.c
+++ w04/e02/flush.c
@@ -2,24 +2,22 @@
 #include <stdio.h>
 
 int main() {
-    int data, flag = 0;
-#pragma omp parallel num_threads(2)
-    {
-        if(omp_get_thread_num() == 0) {
-            /* Write to the data buffer that will be read by thread */
-            data = 42;
-            // #pragma omp flush(data) // data was changed
-            /* Set flag to release thread 1 */
-            flag = 1; // flag was changed
-            // #pragma omp flush(flag)
-        } else if(omp_get_thread_num() == 1) {
-            /* Loop until we see the update to the flag */
-            // #pragma omp flush(flag) // check flag
-            while(flag < 1) {
-            // #pragma omp flush(flag) // check flag
-            }
-            printf("flag=%d data=%d\n", flag, data);
-        }
-    }
-    return 0;
+   int data, flag = 0;
+   #pragma omp parallel num_threads(2)
+   {
+      if (omp_get_thread_num()==0) {
+         /* Write to the data buffer that will be read by thread */
+         data = 42;
+         /* Set flag to release thread 1 */
+         flag = 1;
+      }
+      else if (omp_get_thread_num()==1) {
+         /* Loop until we see the update to the flag */
+         while (flag < 1) {
+         }
+         /* print flag and data */
+         printf("flag=%d data=%d\n", flag, data);
+      }
+   }
+   return 0;
 }
