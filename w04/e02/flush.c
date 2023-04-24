7caa09526dd4a9826df58ddaabbe3b5a028e8748
eprader <56026248+eprader@users.noreply.github.com>
2023-04-24 07:47:23 +0200

    add flush pragmas and util files

--- w04/e02/flush.c
+++ w04/e02/flush.c
@@ -5,16 +5,20 @@ int main() {
     int data, flag = 0;
 #pragma omp parallel num_threads(2)
     {
-        if(omp_get_threads_num() == 0) {
+        if(omp_get_thread_num() == 0) {
             /* Write to the data buffer that will be read by thread */
             data = 42;
+            // #pragma omp flush(data) // data was changed
             /* Set flag to release thread 1 */
-            flag = 1;
+            flag = 1; // flag was changed
+            // #pragma omp flush(flag)
         } else if(omp_get_thread_num() == 1) {
             /* Loop until we see the update to the flag */
+            // #pragma omp flush(flag) // check flag
             while(flag < 1) {
+            // #pragma omp flush(flag) // check flag
             }
-            print("flag=%d data=%d\n", flag, data);
+            printf("flag=%d data=%d\n", flag, data);
         }
     }
     return 0;
